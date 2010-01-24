f799c79215df6fe952cb4151b9f0fb76c5e56ebe
Your Name <bashwork@gmail.com>
2010-01-24 17:28:58 -0600

    changed script to a function

--- 559/homework1/homework1.m
+++ 559/homework1/homework1.m
@@ -3,18 +3,20 @@
 % red eye threshold, and subtract the pixels that match from
 % the red color color portion.
 %
-ey = imread('kelly-redeye.jpg');
-o(:,:,1) = ey(:,:,1) >= 110 & ey(:,:,1) < 255;
-o(:,:,2) = ey(:,:,2) >=   0 & ey(:,:,2) <  45;
-o(:,:,3) = ey(:,:,3) >=   0 & ey(:,:,3) <  70;
-z = o(:,:,1) & o(:,:,2) & o(:,:,3);
-
-subplot(1,3,1);
-imshow(ey);
-
-subplot(1,3,2);
-imshow(z);
-
-ey(:,:,1) = ey(:,:,1) - uint8(z).*ey(:,:,1);
-subplot(1,3,3);
-imshow(ey);
+function homework1(file);
+    ey = imread(file);
+    o(:,:,1) = ey(:,:,1) >= 110 & ey(:,:,1) < 255;
+    o(:,:,2) = ey(:,:,2) >=   0 & ey(:,:,2) <  45;
+    o(:,:,3) = ey(:,:,3) >=   0 & ey(:,:,3) <  70;
+    z = o(:,:,1) & o(:,:,2) & o(:,:,3);
+    
+    subplot(1,3,1);
+    imshow(ey);
+    
+    subplot(1,3,2);
+    imshow(z);
+    
+    ey(:,:,1) = ey(:,:,1) - uint8(z).*ey(:,:,1);
+    subplot(1,3,3);
+    imshow(ey);
+end

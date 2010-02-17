c4507bf6919b6f05f98c7395d5b181bc115cfb27
Your Name <bashwork@gmail.com>
2010-02-16 21:25:59 -0600

    finished initial code

--- 559/project1/stereogram.py
+++ 559/project1/stereogram.py
@@ -1,13 +1,26 @@
 '''
 AutoStereogram Creator
 ----------------------------------------
+
+As of now this supports the following sirds:
+
 - random dot
 - auto-stereogram random dot
 - auto-stereogram texture
-- auto-stereogram ascii
 
+For more information about the algorithm or of stereograms, visit the
+following links:
 
-Information obtained from `Techmind <http://www.techmind.org/stereo/stech.html>`_
+- `Techmind <http://www.techmind.org/stereo/stech.html>`_
+- `Wikipedia <http://en.wikipedia.org/wiki/Autostereogram>`_
+
+Example Useage
+----------------------------------------
+from stereogram import SIRD
+
+v = SIRD("Boxes.jpg")
+j = v.create_random_dot()
+j.show()
 '''
 try: # try for the free speedup
     import psyco
@@ -69,6 +82,31 @@ def get_random_texture():
     data = urllib2.urlopen(url).read()
     return Image.open(cStringIO.StringIO(data))
 
+def create_text_depthmap(message="Hello", width=1024, height=768):
+    ''' Build a depth map with the supplied text which can be
+    used to create a sird.
+
+    TODO work with a collection of messages and position to fit all
+
+    :param message: The message to build a deptmap for
+    :param width: The width of the resulting texture map
+    :param height: The height of the resulting texture map
+    :returns: The resulting random texture map
+    '''
+    from PIL import ImageDraw, ImageFont
+
+    path     = "/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf"
+    padding  = 20
+    size     = int((1.65 * (width - 2*padding)) / len(message))
+    position = (padding, int((height/2) - (size / 2)))
+
+    output   = Image.new('RGB', (width, height))
+    font     = ImageFont.truetype(path, size)
+    draw     = ImageDraw.Draw(output)
+    draw.text(position, message, font=font, fill="#FFFFFF")
+
+    return output
+
 def create_random_texture(width, height):
     ''' Build a random texture map to use for mapping depth
     map points when creating a sird.
@@ -89,10 +127,17 @@ def create_random_texture(width, height):
 class SIRD(object):
     '''
     This class helps create various SIRD images given
-    an appropriate depthmap
+    an appropriate depthmap.
     '''
+    dpi        = 81
+    distance   = dpi * 14
+    separation = dpi * 2.5
+    factor     = 0.55
+    maximum    = dpi * 12
+    minimum    = int((factor * maximum * distance) / ((1 - factor) * maximum + distance))
+    size       = (1024, 768)
 
-    def __init__(self, depthmap, separation=100):
+    def __init__(self, depthmap):
         ''' Initialize a new instance of the SIRD generator
 
         Depthmap can be an image path, uri, or a PIL image. If an image is passed
@@ -101,55 +146,11 @@ class SIRD(object):
 
         :throws IOError: This will throw an IOError if the file does not exist
         :param depthmap: The depthmap to hide in a sird
-        :param separation: The eye separation in pixels
         '''
-        self.separation = separation
         if isinstance(depthmap, str):
            self.depth = Image.open(depthmap)
         else: self.depth = depthmap
-        self.depth = ImageOps.grayscale(self.depth)
-
-    # ------------------------------------------------------------------------- #
-    # Static Private Helper Methods
-    # ------------------------------------------------------------------------- #
-
-    def _create_lookup(self, depth, row):
-        ''' Build a lookup table for the specified row
-    
-        :param depth: The depthmap image to create a lookup for
-        :param row: The row to return a lookup table for
-        :returns: The resulting sird
-        '''
-        row = numpy.asarray(depth)[row]
-        result = range(0, len(row)) # + 1?
-
-        for k,v in enumerate(row):
-            d = self._get_displacement(v)
-            if (d + k) < len(row):
-                result[d + k] = k
-        return result
-
-    def _create(self, depth, texture=None):
-        ''' Build a sird image using random dots to hide the depthmap.
-    
-        :param depth: The depthmap to create a sird for
-        :param texture: The texture map to use or None to use a random one
-        :returns: The resulting sird
-        '''
-        output = Image.new('RGB', depth.size)
-        (dw, dh) = output.size
-
-        # TODO
-        #if not texture:
-        #    texture = create_random_texture(40, 40)
-
-        for h in xrange(dh):
-            lookup = self._create_lookup(depth, h)
-            for w in xrange(dw):
-                if lookup[w] == w: # if the point isn't mapped
-                    output.putpixel((w,h), get_random_color())
-                else: output.putpixel((w,h), output.getpixel((w,h)))
-        return output
+        self.depth = ImageOps.grayscale(self.depth.resize(self.size))
 
     # ------------------------------------------------------------------------- #
     # Private Interface
@@ -167,12 +168,79 @@ class SIRD(object):
         :param color: The gray color to get the displacement for
         :returns: The displacement for the specified color
         '''
-        # max-depth  = max(colors, 0)
-        # pixels     = inches * monitor-ppi
-        # depth      = max - (color * (max - min) / 255)
-        # separation = (eye-sep * depth) / (depth * distance)
+        depth = self.maximum - (color * (self.maximum - self.minimum) / 255)
+        return (self.separation * depth) / (depth + self.distance)
 
-        return self.separation + (color / 2)
+    def _validate(self, points, width, links):
+        ''' This validates that the point is valid from the left
+        and right viewpoint. Based on the validation, the left and
+        right mappings will be updated.
+
+        :param points: The left and right points to validate
+        :param width: The width to validate
+        :param links: The left and right link mappings to update
+        :returns: void
+        '''
+        visible = true;
+        left, right = points
+
+        if (left <= 0) or (right > width):
+            return
+        
+        if links[0][right] != right:
+            if links[0][right] < left:
+                links[1][links[0][right]] = links[0][right];
+                links[0][right] = right;
+            else: visible = false
+
+        if links[1][left] != left:
+            if links[1][left] > right:
+                links[0][links[1][left]] = links[1][left];
+                links[1][left] = left;
+            else: visible = false;
+    
+        if visible:
+            links[0][right] = left;
+            links[1][left]  = right;
+
+    def _create_lookup(self, depth, row):
+        ''' Build a lookup table for the specified row
+    
+        :param depth: The depthmap image to create a lookup for
+        :param row: The row to return a lookup table for
+        :returns: The resulting sird
+        '''
+        row    = depth[row]
+        width  = len(row)
+        result = range(0, width) # initialize with no links
+
+        for k,v in enumerate(row):
+            d = int(self._get_displacement(v) / 2)
+            left, right = k-d, k+d
+            # self._validate((left,right), width, (linkl,linkr))
+            if (left >= 0) and (right < width):
+                result[right] = left
+        return result
+
+    def _create(self, depth, texture):
+        ''' Build a sird image using random dots to hide the depthmap.
+    
+        :param depth: The depthmap to create a sird for
+        :param texture: The texture map to use
+        :returns: The resulting sird
+        '''
+        output = Image.new('RGB', depth.size)
+        (dw, dh) = output.size
+        mapping = numpy.asarray(depth)
+
+        for h in xrange(dh):
+            lookup = self._create_lookup(mapping, h)
+            for w in xrange(dw):
+                if lookup[w] == w: # if the point isn't mapped
+                    output.putpixel((w,h), get_random_color())
+                else:
+                    output.putpixel((w,h), output.getpixel((lookup[w],h)))
+        return output
 
     # ------------------------------------------------------------------------- #
     # Public Interface
@@ -183,7 +251,8 @@ class SIRD(object):
     
         :returns: The resulting sird
         '''
-        return self._create(self.depth)
+        texture = create_random_texture(40, 40)
+        return self._create(self.depth, texture)
     
     def create_textured(self, texture):
         ''' Build a sird image using the specified texture
@@ -201,7 +270,8 @@ class SIRD(object):
         :param frames: The number of frames to create
         :returns: The resulting sird
         '''
-        return (self._create(self.depth.rotate(i*10))
+        texture = create_random_texture(40, 40)
+        return (self._create(self.depth.rotate(i*10), texture)
             for i in xrange(frames))
     
     def create_animated_textured(self, texture, frames=37):
